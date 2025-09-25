3be0c45fc3cca44479f15ff02f83c64bf394a94e
Your Name <you@example.com>
2025-09-25 14:03:24 +0300

    chore: add get-user.js to inspect user role in DB

---
+++ scripts/get-user.js
@@ -0,0 +1,10 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main(){
+  const email = process.argv[2] || 'kiokojackson81@gmail.com';
+  const u = await prisma.user.findUnique({ where: { email: email.toLowerCase() } });
+  console.log(JSON.stringify(u, null, 2));
+}
+
+main().finally(()=>process.exit(0));
