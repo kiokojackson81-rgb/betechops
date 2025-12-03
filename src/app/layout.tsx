ac247c8634c59b6ca5004d34e79df586d5051f3a
Your Name <you@example.com>
2025-12-03 05:26:54 +0300

    fix(auth): provide SessionProvider in app layout (Providers.tsx)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ToastContainer from './_components/ToastContainer';
 import ConfirmProvider from './_components/ConfirmProvider';
+import Providers from './Providers';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Providers>
             <ToastContainer />
             <ConfirmProvider />
-        {children}
+            {children}
+        </Providers>
       </body>
     </html>
   );
